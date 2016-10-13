8f0e9cc55ca478d0e77d5edf085ddfde4fefa454
john <john.dreyfus@y-nov.com>
2016-10-13 12:03:10 +0200

    add logs

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -42,6 +42,6 @@ module.exports = {
             .then(res.created)
             .catch(res.serverError)
     }
-	
+
 };
 
---
+++ config/autoreload.js
@@ -0,0 +1,13 @@
+module.exports.autoreload = {
+  active: true,
+  usePolling: false,
+  dirs: [
+    "api/models",
+    "api/services",
+    "api/controllers",
+    "api/responses",
+    "api/policies",
+    "config/locales"
+  ],
+  ignored: []
+}
