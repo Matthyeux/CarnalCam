0a25169bbbb621bde3cc2d823b78a0cbf7b2d6a2
Matthieu Durindel <matthieu.durindel@gmail.com>
2016-10-13 10:58:03 +0200

    add autoreload

---
+++ config/autoreload.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Matth on 13/10/2016.
+ */
+
+module.exports.autoreload = {
+  active: true,
+  usePolling: false,
+  dirs: [
+    'api/models',
+    'api/controllers',
+    'api/services'
+  ],
+  ignored: []
+};
